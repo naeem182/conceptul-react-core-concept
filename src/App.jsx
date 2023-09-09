c1efaddae39bb678f43f8a53f26ce3cfc6f08d41
naeem <naeemislamjr182@gmail.com>
2023-09-09 14:34:57 +0600

    component

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-
+//comonent
 function Greetings() {
-  return <h2>hey!brother</h2>
+  // return <h2>hey!brother</h2>
+  return (
+    < >
+      <h2>hey!joe</h2>
+      <h3>i wanna ...... </h3>
+      <h1>Describing Zayed khan</h1>
+      <Person></Person>
+      <Message></Message>
+
+    </>
+  )
+}
+function Person() {
+  return <h1>zayed khan</h1>
+}
+function Message() {
+  return <h1>Bangler Tiger</h1>
 }
 function App() {
 
   return (
     <>
 
-      <h3 className='text-center text-3xl'>Hello</h3>
-      <Greetings></Greetings>
+
+      <h3 className='text-center text-3xl'>Hello {5 + 5}</h3>
+      {/* <Greetings></Greetings> */}
       <Greetings />
       <button className=' btn btn-secondary'>click me</button>
     </>
