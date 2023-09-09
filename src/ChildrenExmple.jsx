067b270e05baa04bbe21345eca3878fa3524ee61
naeem <naeemislamjr182@gmail.com>
2023-09-09 21:57:35 +0600

    array rendaring

--- src/ChildrenExmple.jsx
+++ src/ChildrenExmple.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const ChildrenExmple = ({ children }) => {
+    const names = ["naeem", "islam", "junior"]
     return (
         <div>
             {children}
+            {
+                names.map((iteam) =>
+                    (<h2>{iteam}</h2>)
+                )
+            }
         </div>
     )
 }
