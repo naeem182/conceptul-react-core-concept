0fa5d126686815ed7fc5105efd9ba3916c5ee5f1
naeem <naeemislamjr182@gmail.com>
2023-09-09 18:17:02 +0600

    class warning fixed (clasName)

--- src/component/OneCard.jsx
+++ src/component/OneCard.jsx
@@ -13,20 +13,20 @@ const OneCard = (props) => {
     // }
 
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src="https://i.ibb.co/D9wWRM6/olivia.jpg" alt="Shoes" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">
+            <div className="card-body">
+                <h2 className="card-title">
                     {/* {title} */}
-                    <div class="badge badge-secondary">NEW</div>
+                    <div className="badge badge-secondary">NEW</div>
                 </h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions ">
+                <div className="card-actions ">
                     {/* <h2>Views:{myobj.views}</h2>
                     <h2>Views:{plus(100, 200)}k</h2>
                     <h2>Views:100k</h2> */}
-                    <div class="badge badge-outline">Fashion</div>
-                    <div class="badge badge-outline">Products</div>
+                    <div className="badge badge-outline">Fashion</div>
+                    <div className="badge badge-outline">Products</div>
                 </div>
             </div>
         </div>
