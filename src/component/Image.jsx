145c823b30c6e86a9d3831ca44e896fd72bf421f
naeem <naeemislamjr182@gmail.com>
2023-09-09 16:44:21 +0600

    get local image

--- src/component/Image.jsx
+++ src/component/Image.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Image() {
     return (
-        <figure><img src="https://i.ibb.co/D9wWRM6/olivia.jpg" alt="Shoes" /></figure>
+        <figure>
+            {/* <img src="https://i.ibb.co/D9wWRM6/olivia.jpg" alt="Shoes" /> */}
+            <img src="./images/nn.jpg" alt="Shoes" />
+        </figure>
 
     )
 }
