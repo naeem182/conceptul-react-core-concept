17cd1ac20d75bb367bd84fc8154058757666c944
naeem <naeemislamjr182@gmail.com>
2023-09-09 17:28:04 +0600

    js expression

--- src/component/OneCard.jsx
+++ src/component/OneCard.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 
 const OneCard = () => {
 
-    const title = "hell joe"
+    const title = "hell joe";
+    const myobj = {
+        views: " 120k",
 
+    };
+    function plus(a, b) {
+        return a + b;
+    }
 
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
             <figure><img src="https://i.ibb.co/D9wWRM6/olivia.jpg" alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">
-                    Shape of You
+                    {title}
                     <div class="badge badge-secondary">NEW</div>
                 </h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify">
-                    <h2>Views:100k</h2>
+                <div class="card-actions ">
+                    <h2>Views:{myobj.views}</h2>
+                    <h2>Views:{plus(100, 200)}k</h2>
+                    {/* <h2>Views:100k</h2> */}
                     <div class="badge badge-outline">Fashion</div>
                     <div class="badge badge-outline">Products</div>
                 </div>
